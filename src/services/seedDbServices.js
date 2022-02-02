c944ce3b57e5fd0f45d7eaa6b998760389d610e5
Nicolas <nico.06.89crc@gmail.com>
2022-02-02 15:37:25 -0300

    Updating the database seed process with new data.

--- src/services/seedDbServices.js
+++ src/services/seedDbServices.js
@@ -75,7 +75,7 @@ const seedProducts = async (usersCreated, tagsCreated) => {
   const productsCreated = [];
 
   for (product of productsData) {
-    if (product.category_slug) {
+    if (product.category_slug !== null) {
       const category = await Category.findOne({ slug: product.category_slug });
 
       const productAux = {
